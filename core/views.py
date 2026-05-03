b23fc77d5cdfdff2dd9d9e397a6667975216330e
Andres Lopez <imanolrodriguez124@gmail.com>
2026-05-03 18:19:31 -0500

    Página de detalles de producto

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 
 def home(request):
     productos = Producto.objects.all()
     return render(request, 'core/home.html', {'productos': productos})
+
+def detalle_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, 'core/detalle.html', {'producto': producto})
